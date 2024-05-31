d6cb06ba7a203ff6ae0ee75bf15f761d41bc29b5
apoorav-987 <apoorav1326.be22@chitkara.edu.in>
2024-05-31 16:10:38 +0530

    1st

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Grid, Box, Button } from '@mui/material';
+import styled from 'styled-components';
+import Students from "../assets/students.svg";
+import { LightPurpleButton } from '../components/buttonStyles';
+
+const Homepage = () => {
+    return (
+        <StyledContainer>
+            <Grid container spacing={0}>
+                <Grid item xs={12} md={6}>
+                    <img src={Students} alt="students" style={{ width: '100%' }} />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <StyledPaper elevation={3}>
+                        <StyledTitle>
+                            Welcome to
+                            <br />
+                            CampusSync
+                            <br />
+                        </StyledTitle>
+                        <StyledText>
+                        Effortlessly streamline college management, organize classes, and efficiently add both students and faculty with our comprehensive platform. 
+                        <br /><br />
+                        With easy access to records, you can view marks and grades at a glance, ensuring that you stay informed about each student's progress. Communication is made simple and effective, bridging the gap between students, faculty, and administration.
+                        <br /><br /> 
+                        Our platform ensures a smooth and productive academic environment, fostering better engagement and collaboration across the board.
+                        </StyledText>
+                        <StyledBox>
+                            <StyledLink to="/choose">
+                                <LightPurpleButton variant="contained" fullWidth>
+                                    Login
+                                </LightPurpleButton>
+                            </StyledLink>
+                            {/* <StyledLink to="/chooseasguest">
+                                <Button variant="outlined" fullWidth
+                                    sx={{ mt: 2, mb: 3, color: "#7f56da", borderColor: "#7f56da" }}
+                                >
+                                    Login as Guest
+                                </Button>
+                            </StyledLink> */}
+                            <StyledText>
+                                Don't have an account?{' '}
+                                <Link to="/Adminregister" style={{color:"#550080"}}>
+                                    Sign up
+                                </Link>
+                            </StyledText>
+                        </StyledBox>
+                    </StyledPaper>
+                </Grid>
+            </Grid>
+        </StyledContainer>
+    );
+};
+
+export default Homepage;
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const StyledPaper = styled.div`
+  padding: 24px;
+  height: 100vh;
+`;
+
+const StyledBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content:center;
+  gap: 16px;
+  padding: 24px;
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 3rem;
+  color: #252525;
+  /* font-family: "Manrope"; */
+  font-weight: bold;
+  padding-top: 0;
+  letter-spacing: normal;
+  line-height: normal;
+`;
+
+const StyledText = styled.p`
+  /* color: #550080; */
+  margin-top: 30px;
+  margin-bottom: 30px; 
+  letter-spacing: normal;
+  line-height: normal;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
